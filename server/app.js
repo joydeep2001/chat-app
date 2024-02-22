654f59a72651a457b2bad00937ec4ab33dd6e947
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-02-22 23:39:12 +0530

    env setup added and express boiler plate added.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,28 @@
-console.log("hello world!!");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cookieparser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+
+
+dotenv.config();
+const cors = require("cors");
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true });
+const connect = mongoose.connection;
+connect.on("open", () => {
+  console.log("conneted to Database");
+});
+
+app.use(cookieparser());
+app.use(express.json());
+
+let PORT = process.env.PORT || 3005;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is Running ${PORT}`);
+  });
+
