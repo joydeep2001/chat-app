d2ad31f0134cf6e8b371bfd965b01b593630b38f
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-02-22 23:30:43 +0530

    user model refactor.

--- server/models/user.js
+++ server/models/user.js
@@ -22,42 +22,5 @@ const usrSchema = mongoose.Schema({
     }
 })
 
-const messageSchema = mongoose.Schema({
-    id:{
-        type:String
-    },
-    sender: {
-        type:String,
-    },
-    message_type: {
-        type:String
-    },
-    content:{
-        type:String
-    }, 
-    url: {
-        type:String
-    },
-    group_id:{
-        type:String
-    },
-    reciver_id:{
-        type:String,
-    }
-})
-
-const groupSchema =  mongoose.Schema({
-    id:{
-        type:String,
-    },
-    members:{
-        type:[String]
-    },
-    admin:{
-        type:[String]
-    }
-})
-
-
 
 module.exports = mongoose.model('User', usrSchema);;
