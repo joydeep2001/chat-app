fe21bda89d918d2804d2b25ed9a43d55003659c5
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-03-14 21:00:25 +0530

    Hooks: Created hook for ajax calls to server.

---
+++ client/src/hooks/useAxiosWrapper.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function useAxiosWrapper() {
+  const [data, setData] = useState({});
+  useEffect(() => console.log(data), [data])
+  function fetchData(endpoint, config) {
+    const url = "http://localhost:3005/api/v1" + endpoint;
+    axios(url, { ...config, withCredentials: true })
+      .then((response) => {
+        setData({ ...response.data, success: true });
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        switch (error.code) {
+          case "ERR_BAD_REQUEST":
+            alert(error.response.data.message);
+            break;
+          default:
+            alert("Something went wrong! check console.");
+            console.log(error);
+            break;
+        }
+      });
+  }
+
+  return { data, fetchData };
+}
